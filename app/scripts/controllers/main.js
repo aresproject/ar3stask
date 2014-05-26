bfe41b30ec9277a2061c4b6b5e0ac2454def40e9
aresproject <aresproj3ct@gmail.com>
2014-05-26 10:33:18 +0300

    RE SCAFFOLD THE PROJECT

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ar3staskApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
